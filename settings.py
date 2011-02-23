50f7002a2e2f66054d7f928d2f4108a2793d87c3
dvilchez <dvilchezm@gmail.com>
2011-02-23 20:27:04 +0100

    pasa tags por get y encuentra el view

--- settings.py
+++ settings.py
@@ -87,7 +87,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'portags',
-    'django.contrib.admin'
+    'django.contrib.admin',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
